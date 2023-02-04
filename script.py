e89d016c3fe81b0b327e21c1a7adbcd28742adb9
Conor Neilson <condwanaland@gmail.com>
2023-02-04 18:03:27 +1300

    testing over, gone live

--- script.py
+++ script.py
@@ -25,8 +25,8 @@ q_no = next_q["next_question"]
 
 body = questions["questions"][q_no]
 
-v_message = F.init_email(SENDER_EMAIL, "condwanaland@gmail.com", q_no, body)
-i_message = F.init_email(SENDER_EMAIL, "condwanaland@gmail.com", q_no, body)
+v_message = F.init_email(SENDER_EMAIL, V_EMAIL, q_no, body)
+i_message = F.init_email(SENDER_EMAIL, I_EMAIL, q_no, body)
 
 F.send_email(v_message, SENDER_EMAIL, SENDER_PWORD) 
 F.send_email(i_message, SENDER_EMAIL, SENDER_PWORD) 
